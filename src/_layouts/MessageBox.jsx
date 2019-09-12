dc9a707354a0d90b8895d50337a4bf877fba0bb4
phdumaresq <phdumaresq@gmail.com>
2019-09-12 15:02:44 -0400

    Added sending messages - dissapear as soon as you refresh

--- src/_layouts/MessageBox.jsx
+++ src/_layouts/MessageBox.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import Messages from './Messages';
+import sendMessage from '../lib/sendMessage'
 
 export default function MessageBox() {
+  function send(evt) {
+    if (evt.key == "Enter") {
+      evt.preventDefault();
+      const channel = "general";
+      const sender = "nixin72";
+      const body = evt.target.value;
+
+      sendMessage(channel, sender, body);
+      evt.target.value = "";
+      ReactDOM.render(<Messages />, document.getElementById("messageArea"));
+    }
+  }
+
   return (
     <div className="messageBox">
-      <textarea rows="1" tabIndex="1" placeholder="Message #{{ channelName }}"></textarea>
+      <textarea id="sendMsg"
+                onKeyDown={ send }
+                rows="1" tabIndex="1"
+                placeholder="Message #{{ channelName }}"></textarea>
       <div>
         <img src="https://www.sccpre.cat/mypng/full/77-774992_black-shape-paper-plane-paper-airplane-icon-instagram.png"
              width="30"
