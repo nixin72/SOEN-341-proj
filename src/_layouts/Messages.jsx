621072620daa867fd2a818524c43c13aab0de4c2
phdumaresq <phdumaresq@gmail.com>
2019-10-10 00:09:45 -0400

    Turned Messages.jsx into a class to allow lifecycle method. This is to get it to update when new messages are detected. Uses timeout instead of websockets

--- src/_layouts/Messages.jsx
+++ src/_layouts/Messages.jsx
@@ -1,29 +1,53 @@
 import React from 'react';
 import Message from './Message';
 import axios from 'axios';
-import messages from '../models/messages.json'
 
 export default class Messages extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: [],
+      channel: props.channel
+    }
+
+    this.willUpdate = false;
+
+    axios.get("http://localhost:3001/messages?channel=" + props.channel)
+      .then(data => {
+        this.setState({ messages: data.data });
+      })
+      .catch(err => {
+        console.error("ERROR", err)
+      });
+  }
+
   createMessageComponents(messages) {
-    return Object.keys(messages["general"]).map(m =>
-      <Message time={ (d =>
-        `${d.getHours()}:${d.getMinutes()}`)(new Date(parseInt(m))) }
-               sender={ messages["general"][m].sender }
-               body={ messages["general"][m].body }/>)
+    return Object.keys(messages).map(m =>
+      <Message time={(d =>
+        `${d.getHours()}:${d.getMinutes()}`)(new Date(parseInt(m)))}
+        sender={messages[m].sender}
+        body={messages[m].body} />)
   }
 
-  getMessages() {
-    // return axios.get("http://localhost:3001/messages")
-    //   .then(m => createMessageComponents(m));
-
-    return this.createMessageComponents(messages);
-
+  shouldComponentUpdate() {
+    return this.willUpdate;
   }
 
-  render () {
+  componentDidMount() {
+    setInterval(() => {
+      axios.get(`http://localhost:3001/messages/latest?timestamp=${Date.now()}&channel=${this.props.channel}`)
+        .then(data => {
+          console.log(data);
+          this.willUpdate = data.data.update;
+          console.log(this.willUpdate);
+        })
+    }, 1000);
+  }
+
+  render() {
     return (
       <div className="messages">
-      { this.getMessages() }
+        {this.createMessageComponents(this.state.messages)}
       </div>
     );
   }
