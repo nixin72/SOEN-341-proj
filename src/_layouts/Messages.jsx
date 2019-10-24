b774b03752bf17009684d3a02d7598f77bf95077
phdumaresq <phdumaresq@gmail.com>
2019-10-24 11:19:24 -0400

    Added updating to messages so you can send and receive

--- src/_layouts/Messages.jsx
+++ src/_layouts/Messages.jsx
@@ -1,54 +1,37 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Message from './Message';
 import axios from 'axios';
 
-export default class Messages extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      messages: [],
-      channel: props.channel
-    }
+export default function Messages(props) {
+  let messages = [];
 
-    this.willUpdate = false;
+  setInterval(() => {
+    messages = getMessages(props.channel);
+    messages.then(data => {
+      ReactDOM.render(
+        <div className="messages">
+          {createMessageComponents(data.data)}
+        </div>,
+        document.getElementById('messageArea')
+      )
+    })
+  }, 1000)
 
-    axios.get("http://localhost:3001/messages?channel=" + props.channel)
-      .then(data => {
-        this.setState({ messages: data.data });
-      })
-      .catch(err => {
-        console.error("ERROR", err)
-      });
-  }
+  return (
+    <div className="messages">
+    </div>
+  )
+}
 
-  createMessageComponents(messages) {
-    return Object.keys(messages).map(m =>
-      <Message time={(d =>
-        `${d.getHours()}:${d.getMinutes()}`)(new Date(parseInt(m)))}
-        sender={messages[m].sender}
-        body={messages[m].body} />)
-  }
+function getMessages(channel) {
+  return axios.get("http://localhost:3001/messages?channel=general&" + channel)
+}
 
-  shouldComponentUpdate() {
-    return this.willUpdate;
-  }
-
-  componentDidMount() {
-    setInterval(() => {
-      axios.get(`http://localhost:3001/messages/latest?timestamp=${Date.now()}&channel=${this.props.channel}`)
-        .then(data => {
-          console.log(data);
-          this.willUpdate = data.data.update;
-          console.log(this.willUpdate);
-        })
-    }, 1000);
-  }
-
-  render() {
-    return (
-      <div className="messages">
-        {this.createMessageComponents(this.state.messages)}
-      </div>
-    );
-  }
+function createMessageComponents(messages) {
+  return Object.keys(messages).map(m =>
+    <Message time={(d =>
+      `${d.getHours()}:${d.getMinutes()}`)(new Date(parseInt(m)))}
+      sender={messages[m].sender}
+      body={messages[m].body} />)
 }
